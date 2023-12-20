0d02e819bc0f7602345eed89f3a787257c8ea10c
Daichi Kimura <dkimura@bdd.xyz>
2023-12-21 00:33:18 +0900

    style: improve button styling

--- packages/core/src/components/button.tsx
+++ packages/core/src/components/button.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { css } from '../../styled-system/css'
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (props, ref) => (
-    <button ref={ref} type="button" {...props}>
+    <button
+      ref={ref}
+      type="button"
+      className={css({
+        bg: 'brand',
+        color: 'brand',
+        fontFamily: 'Inter',
+        px: '4',
+        py: '3',
+        borderRadius: 'md',
+        _hover: { bg: 'red.400' },
+      })}
+      {...props}
+    >
       {props.children}
     </button>
   )
